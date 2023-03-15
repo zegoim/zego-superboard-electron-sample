c357e4553dcf3924d6eff361fbcd0c74a417d08d
ttzhang <ttzhang@zego.im>
2023-03-15 18:31:18 +0800

    🔨新增刷新按钮

--- js/login/demo.js
+++ js/login/demo.js
@@ -2,17 +2,17 @@
  *This code block is needed when the ZEGOCLOUD web demo runs. It does not need to be concerned when the demo is opened.
  */
 
-if (location.port === 4003) {
-  // Obtain the SDK version.
-  function getSDKVersionOptions(list) {
-    return list
-      .map(function (v) {
-        return `<option value="${v}">${v}</option>`;
-      })
-      .join('');
-  }
-  // SDK version settings
-  $('body').append(`
+if (location.port === '4003') {
+    // Obtain the SDK version.
+    function getSDKVersionOptions(list) {
+        return list
+            .map(function(v) {
+                return `<option value="${v}">${v}</option>`;
+            })
+            .join('');
+    }
+    // SDK version settings
+    $('body').append(`
     <div class="modal fade" id="versionModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -43,73 +43,75 @@ if (location.port === 4003) {
     </div>
   `);
 
-  $('#login-page').append(`
-  <span id="sdk-version" data-toggle="modal" data-target="#versionModal">SDK 版本（修改版本，demo的SDK版本为2.11.0）</span>`)
+    $('#login-page').append(`
+  <span id="sdk-version" data-toggle="modal" data-target="#versionModal">SDK 版本（修改版本，demo的SDK版本为2.11.0）</span>`);
 
-  $('#reload-btn').click(()=>{
-    console.log('===reload', window.name)
-    alert('===reload')
-  })
-
-  $.ajaxSettings.async = false;
-  $.get(
-    'http://zego-public.oss-cn-shanghai.aliyuncs.com/goclass/sdk/sdkVer.json', {},
-    function (res) {
-      console.log(res);
-      $('#superBoard-list').html(getSDKVersionOptions(res.superboard));
-    },
-    'json'
-  );
-  $.ajaxSettings.async = true;
-
-  var base_sdk_url = `http://zego-public.oss-cn-shanghai.aliyuncs.com/goclass/sdk/superboard/`
-
-  layui.use(['layer', 'jquery', 'form'], function () {
-    var layer = layui.layer,
-      $ = layui.jquery,
-      form = layui.form;
-
-    form.on('select(superBoardVersion)', function (data) {
-      console.log(data.value);
-      // Perform rendering one more time.
-      form.render('select');
+    $('#login-page').append(
+        `<button type="button" class="btn btn-primary btn-xs" style="position: absolute;top: 10px;right:10px">刷新</button>`
+    );
+    $('#reload-btn').click(() => {
+        window.location.reload();
     });
-    form.on('submit(versionForm)', function(data){
-      console.log('===data', data.field)
-      sessionStorage.setItem('superBoardVersion', JSON.stringify(data.field.superBoardVersion));
-      // Replace the SDK path.
-      loadScript(base_sdk_url + data.field.superBoardVersion);
-      return false;
+
+    $.ajaxSettings.async = false;
+    $.get(
+        'http://zego-public.oss-cn-shanghai.aliyuncs.com/goclass/sdk/sdkVer.json',
+        {},
+        function(res) {
+            console.log(res);
+            $('#superBoard-list').html(getSDKVersionOptions(res.superboard));
+        },
+        'json'
+    );
+    $.ajaxSettings.async = true;
+
+    var base_sdk_url = `http://zego-public.oss-cn-shanghai.aliyuncs.com/goclass/sdk/superboard/`;
+
+    layui.use(['layer', 'jquery', 'form'], function() {
+        var layer = layui.layer,
+            $ = layui.jquery,
+            form = layui.form;
+
+        form.on('select(superBoardVersion)', function(data) {
+            console.log(data.value);
+            // Perform rendering one more time.
+            form.render('select');
+        });
+        form.on('submit(versionForm)', function(data) {
+            console.log('===data', data.field);
+            sessionStorage.setItem('superBoardVersion', JSON.stringify(data.field.superBoardVersion));
+            // Replace the SDK path.
+            loadScript(base_sdk_url + data.field.superBoardVersion);
+            return false;
+        });
     });
-  });
 
-  function loadScript(url) {
-    return new Promise(function (resolve) {
-      var head = document.getElementsByTagName('head')[0];
-      var script = document.createElement('script');
+    function loadScript(url) {
+        return new Promise(function(resolve) {
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
 
-      script.type = 'text/javascript';
-      script.src = url;
-      if (script.readyState) {
-        // IE
-        script.onreadystatechange = function () {
-          if (script.readyState == 'loaded' || script.readyState == 'complete') {
-            script.onreadystatechange = null;
-          }
-        };
-      } else {
-        // Others
-        script.onload = function () {
-          resolve();
-        };
-      }
-      head.appendChild(script);
-    });
-  }
+            script.type = 'text/javascript';
+            script.src = url;
+            if (script.readyState) {
+                // IE
+                script.onreadystatechange = function() {
+                    if (script.readyState == 'loaded' || script.readyState == 'complete') {
+                        script.onreadystatechange = null;
+                    }
+                };
+            } else {
+                // Others
+                script.onload = function() {
+                    resolve();
+                };
+            }
+            head.appendChild(script);
+        });
+    }
 
-  var sdkver = JSON.parse(sessionStorage.getItem('superBoardVersion'))
-  console.warn('===sdkver', sdkver)
-  // Replace the SDK path.
-  sdkver ? loadScript(base_sdk_url + sdkver) : loadScript(base_sdk_url + $('#superBoardVersion').val());
-
+    var sdkver = JSON.parse(sessionStorage.getItem('superBoardVersion'));
+    console.warn('===sdkver', sdkver);
+    // Replace the SDK path.
+    sdkver ? loadScript(base_sdk_url + sdkver) : loadScript(base_sdk_url + $('#superBoardVersion').val());
 }

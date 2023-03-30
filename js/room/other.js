f4fcde50bceab7243d306675d83c89612d7110de
yeozhuang <yeozhuang@zego.im>
2023-03-30 16:51:48 +0800

    🔥删除调试代码；

--- js/room/other.js
+++ js/room/other.js
@@ -57,14 +57,13 @@ function redo() {
  */
 function snapshot() {
     var zegoSuperBoardSubView = zegoSuperBoard.getSuperBoardView().getCurrentSuperBoardSubView();
-    roomUtils.toast('snapshot!!',!!zegoSuperBoardSubView)
-    zegoSuperBoardSubView &&
-        zegoSuperBoardSubView.snapshot().then(function (data) {
-            roomUtils.toast(data)
+    zegoSuperBoardSubView&&zegoSuperBoardSubView.snapshot().then(function (data) {
             var link = document.createElement('a');
             link.href = data.image;
             link.download = zegoSuperBoardSubView.getModel().name + saveImg++ + '.png';
             link.click();
+        }).catch((res)=>{
+            console.error(res)
         });
 }
 
